61329457ad7eb64abfada7499dfc87eb49f34b02
ducdh1210 <ducdh1210@gmail.com>
2016-07-05 10:45:18 -0500

    add built-in igraph's community detection functions

--- dmi_analysis.R
+++ dmi_analysis.R
@@ -77,15 +77,15 @@ pp2_adjM = as.matrix(pp2_adjMatrix)
 # For input, we use the reciprocal of the adjacency matrix; hierarchical
 # clustering works by comparing the _distance_ between objects instead of the
 # _similarity_.
-gene_tree <- hclust(as.dist(1 - pp2_adjM), method="average")
+gene_tree = hclust(as.dist(1 - pp2_adjM), method="average")
 gc()
 # we will use the cuttreeDynamicTree method to break apart the hc dendrogram
 # into separate modules
-module_labels <- cutreeDynamicTree(dendro=gene_tree, minModuleSize=15,
+module_labels = cutreeDynamicTree(dendro=gene_tree, minModuleSize=15,
                                    deepSplit=TRUE)
 gc()
 # assign a color to each module for easier visualization and referencing
-module_colors <- labels2colors(module_labels);
+module_colors = labels2colors(module_labels);
 
 nlevels(as.factor(module_colors)) # [1] 339 ==> thus 338 modules 
 
@@ -97,7 +97,7 @@ write.graph(graph = pp2_igraph, file = file_name, format='graphml') # able to di
 #### ---------------- MCODE ---------------------------------------------------------------
 library(ProNet)
 
-result <- mcode(pp2_igraph,vwp=0.05,haircut=TRUE,fluff=FALSE,fdt=0.8,loops=FALSE)
+result = mcode(pp2_igraph,vwp=0.05,haircut=TRUE,fluff=FALSE,fdt=0.8,loops=FALSE)
 mcode_result = result; 
 rm(result)
 
@@ -105,7 +105,7 @@ rm(result)
 library(igraph)
 # This function tries to find dense subgraph, also called communities in graphs 
 # via directly optimizing a modularity score.
-ppi2_fastgreedy <- fastgreedy.community(pp2_igraph, weights = E(pp2_igraph)$weight)
+ppi2_fastgreedy = fastgreedy.community(pp2_igraph, weights = E(pp2_igraph)$weight)
 attributes(ppi2_fastgreedy)
 modularity(ppi2_fastgreedy) # [1] 0.4127827
 communities(ppi2_fastgreedy)
@@ -121,7 +121,7 @@ library(igraph)
 # in a graph via random walks. The idea is that short random walks tend to stay 
 # in the same community.
 # http://arxiv.org/abs/physics/0512106
-ppi2_walktrap <- walktrap.community(pp2_igraph, weights = E(pp2_igraph)$weight)
+ppi2_walktrap = walktrap.community(pp2_igraph, weights = E(pp2_igraph)$weight)
 attributes(ppi2_walktrap)
 membership(ppi2_walktrap) 
 table(membership(ppi2_walktrap)) # 2364 modules
@@ -131,7 +131,7 @@ library(igraph)
 # This function tries to find densely connected subgraphs in a graph by calculating the 
 # leading non-negative eigenvector of the modularity matrix of the graph.
 # https://arxiv.org/abs/physics/0605087
-ppi2_leadingEigenvector <- leading.eigenvector.community(pp2_igraph, weights = E(pp2_igraph)$weight)
+ppi2_leadingEigenvector = leading.eigenvector.community(pp2_igraph, weights = E(pp2_igraph)$weight)
 module_matrix_leadingEigenvector = getModuleMatrix(igraph_object = pp2_igraph, 
                                            community_membership = membership(ppi2_leadingEigenvector))
 
@@ -141,7 +141,7 @@ library(igraph)
 # In works by labeling the vertices with unique labels and then updating the labels 
 # by majority voting in the neighborhood of the vertex.
 # https://arxiv.org/pdf/0709.2938.pdf
-ppi2_labelPropagation <- label.propagation.community(pp2_igraph, weights = E(pp2_igraph)$weight)
+ppi2_labelPropagation = label.propagation.community(pp2_igraph, weights = E(pp2_igraph)$weight)
 attributes(ppi2_labelPropagation)
 membership(ppi2_labelPropagation) 
 table(membership(ppi2_labelPropagation)) # 92 modules
@@ -151,7 +151,7 @@ library(igraph)
 # Find community structure that minimizes the expected description length
 # of a random walker trajectory
 # http://arxiv.org/abs/0707.0609
-ppi2_infomap <- infomap.community(pp2_igraph, e.weights = E(pp2_igraph)$weight)
+ppi2_infomap = infomap.community(pp2_igraph, e.weights = E(pp2_igraph)$weight)
 membership(ppi2_infomap) 
 table(membership(ppi2_infomap)) # 711 modules
 
@@ -161,9 +161,9 @@ library(igraph)
 # finding community structure, see references below. It is based on the modularity
 # measure and a hierarchial approach.
 # https://arxiv.org/abs/0803.0476
-ppi2_multilevel <- multilevel.community(pp2_igraph, weights = E(pp2_igraph)$weight)
-membership(ppi2_infomap) 
-table(membership(ppi2_infomap)) # 711 modules
+ppi2_multilevel = multilevel.community(pp2_igraph, weights = E(pp2_igraph)$weight)
+membership(ppi2_multilevel) 
+table(membership(ppi2_multilevel)) # 61 modules
 
 #### ---------------- UTILITY FUNCTIONS -------------------------------------------
 
