c1b4bbaac04dac906d523dc3cdeab693629c147d
ducdh1210 <ducdh1210@gmail.com>
2016-07-08 16:00:38 -0500

    test43

--- dmi_analysis.R
+++ dmi_analysis.R
@@ -262,7 +262,7 @@ save(cliq, file="cliques2_sub.RData")
 
 table(cliq_sizes)
 
-# test2
+# test43
 
 # cliq_sizes
 # 5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25   26   27 
